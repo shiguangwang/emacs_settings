a50b5489cb2960fb19d473a27e06a6a42f9ebc01
Shiguang Wang <sgwang@fb.com>
2016-08-28 15:39:03 -0700

    Added org (2).

---
+++ emacs.d/elpa/org-beautify-theme-20150106.956/org-beautify-theme-autoloads.el
@@ -0,0 +1,16 @@
+;;; org-beautify-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("org-beautify-theme.el") (22467 26439
+;;;;;;  792104 27000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-beautify-theme-autoloads.el ends here
